7a9f24c274b9a453a6d934884ce7e97248330205
Chenzhang Wang <cwang@hlw.com>
2022-03-09 23:58:38 -0500

    Fix setting model bug.

--- BIM 360 File Transfer/BIM360FileTransfer/Models/Settings.cs
+++ BIM 360 File Transfer/BIM360FileTransfer/Models/Settings.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BIM360FileTransfer.Models
 {
-    internal static class Setttings
+    internal static class Settings
     {
         public static string JSON_PATH { get; set; } = "";
         public static bool CREATE_NEW_VERSION { get; set; } = true;
